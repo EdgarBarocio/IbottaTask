86fad5f0371e90969f5038ec016451d023800191
Edgar Barocio <edgar.barocio@gmail.com>
2023-06-28 19:31:41 -0400

    Initial commit to create project, Add json with data to resources, Create model for offers contained in json file

---
+++ IbottaApp/IbottaApp/Models/OffersModel.swift
@@ -0,0 +1,26 @@
+//
+//  OffersModel.swift
+//  IbottaApp
+//
+//  Created by Edgar Barocio on 6/28/23.
+//
+
+import Foundation
+
+struct OffersModel: Codable {
+    var id: String
+    var url: String
+    var name: String
+    var description: String
+    var terms: String
+    var currentValue: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case url
+        case name
+        case description
+        case terms
+        case currentValue = "current_value"
+    }
+}
