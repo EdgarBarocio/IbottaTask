9b96609af5331ffe13c8adad69093250ac5fd35c
Edgar Barocio <edgar.barocio@gmail.com>
2023-06-28 19:49:06 -0400

    A little eye candy for the splash screen Add loadView function to create all subViews that will be used on the project

--- IbottaApp/IbottaApp/ViewController.swift
+++ IbottaApp/IbottaApp/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var sampleLabel: UILabel!
+    
+    //Custom load view to create everything programatically instead of using Storyboards
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .lightGray
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
