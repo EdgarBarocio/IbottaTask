c659ac3d2a1c14bd1cc0cdbc836bbd2c6922120f
Edgar Barocio <edgar.barocio@gmail.com>
2023-06-28 21:22:27 -0400

    Programatic creation of Collection View to present offers Update parser to return decoded offers as an array of Offers

--- IbottaApp/IbottaApp/DataParsingWorker.swift
+++ IbottaApp/IbottaApp/DataParsingWorker.swift
@@ -9,16 +9,19 @@ import Foundation
 
 class DataParsingWorker {
     
-    func parseJSONFromFile() {
+    func parseJSONFromFile() -> [OffersModel]? {
         
         if let url = Bundle.main.url(forResource: "Offers", withExtension: "json") {
             do {
                 let data = try Data(contentsOf: url)
                 let decoder = JSONDecoder()
                 let decoded = try decoder.decode([OffersModel].self, from: data)
+                return decoded
             } catch {
                 print(error)
             }
         }
+        
+        return nil
     }
 }
--- IbottaApp/IbottaApp/ViewController.swift
+++ IbottaApp/IbottaApp/ViewController.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var sampleLabel: UILabel!
+    var offersCollectionView: UICollectionView!
     
     //Custom load view to create everything programatically instead of using Storyboards
     override func loadView() {
         view = UIView()
-        view.backgroundColor = .lightGray
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        buildCollectionView()
     }
     
     override func viewDidLoad() {
@@ -23,5 +28,41 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    private func buildCollectionView() {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 60, height: 60)
+        
+        /**
+            var layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+            layout.itemSize = CGSizeMake(<width>, <height>)
+            // Setting the space between cells
+            layout.minimumInteritemSpacing = <Space between columns>
+            layout.minimumLineSpacing = <Space between rows>
+         **/
+        
+        self.offersCollectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        self.offersCollectionView.dataSource = self
+        self.offersCollectionView.delegate = self
+        self.offersCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "OfferCell")
+        self.offersCollectionView.backgroundColor = UIColor.white
+        view.addSubview(self.offersCollectionView)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "OfferCell", for: indexPath)
+               myCell.backgroundColor = UIColor.blue
+               return myCell
+    }
+    
+    
 }
 
